ab185cb390952fc1778350a228bd40eb0ee98954
hks24 <74139697+hks24@users.noreply.github.com>
2021-04-17 15:47:52 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,28 @@
+class Rope{
+    constructor(bodyA,pointB){
+       var options = {
+       bodyA : bodyA,
+       pointB : pointB,
+       stifness:10,
+       length:5
+}
+this.pointB = pointB
+this.chain = Constraint.create(options);
+World.add(world,this.chain);
+
+
+}
+display(){
+    if(this.chain.bodyA){
+    var pointA = this.chain.bodyA.position;
+    var pointB = this.pointB;
+   
+
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+}
+
+}
+fly(){
+   this.chain.bodyA = null;
+}
+}
